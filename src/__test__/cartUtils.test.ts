1b2ed43a91af7b9ad58a41d9fc7b9bcec1776548
Ryan Holman <r.holman@students.clark.edu>
2026-01-19 03:58:34 -0800

    Finished part 4

--- src/__test__/cartUtils.test.ts
+++ src/__test__/cartUtils.test.ts
@@ -30,54 +30,193 @@ describe("applyDiscount", () => {
             "Discount cannot exceed 100%"
         );
     });
+});
 
-    describe("calculateTax", () => {
-        it("calculates tax on a price", () => {
-            expect(calculateTax(100, 8.5)).toBeCloseTo(8.5, 2);
-        });
-        
-        it("returns 0 tax when rate is 0%", () => {
-            expect(calculateTax(50, 0)).toBe(0);
-        });
-
-        it("handles decimal prices correctly", () => {
-            expect(calculateTax(19.99, 10)).toBeCloseTo(2.0, 2);
-        });
-
-        it("returns 0 tax when item is tex-exempt", () => {
-            expect(calculateTax(100, 8.5, true)).toBe(0);
-        });
-
-        it("throws an error for negative prices", () => {
-            expect(() => calculateTax(-10, 8.5)).toThrow("Price cannot be negative");
-        });
-
-        it("throws and error for negative tax rates", () => {
-            expect(() => calculateTax(100, -5)).toThrow("Tax rate cannot be negative");
-        });
+describe("calculateTax", () => {
+    it("calculates tax on a price", () => {
+        expect(calculateTax(100, 8.5)).toBeCloseTo(8.5, 2);
+    });
+    
+    it("returns 0 tax when rate is 0%", () => {
+        expect(calculateTax(50, 0)).toBe(0);
     });
 
-    describe("calculateTotal", () => {
-        // TODO: Add at least 6 test cases
-        // Consider: single item, multiple items, discounts, tax-exempt items,
-        // emtpy cart, mixed tax-exempt and taxable items
+    it("handles decimal prices correctly", () => {
+        expect(calculateTax(19.99, 10)).toBeCloseTo(2.0, 2);
+    });
 
-        it("calculates totals for a sigle item", () => {
-            
-        });
+    it("returns 0 tax when item is tex-exempt", () => {
+        expect(calculateTax(100, 8.5, true)).toBe(0);
+    });
 
-        it("calculates totals for multiple items", () => {
+    it("throws an error for negative prices", () => {
+        expect(() => calculateTax(-10, 8.5)).toThrow("Price cannot be negative");
+    });
 
-        });
+    it("throws and error for negative tax rates", () => {
+        expect(() => calculateTax(100, -5)).toThrow("Tax rate cannot be negative");
+    });
+});
 
-        it("applies discount before calculateing tax", () => {
+describe("calculateTotal", () => {
+    // TODO: Add at least 6 test cases
+    // Consider: single item, multiple items, discounts, tax-exempt items,
+    // empty cart, mixed tax-exempt and taxable items
 
-        });
+    it("calculates totals for a single item", () => {
+        const listItems: CartItem[] = [
+            {
+                price: 4,
+                quantity: 1,
+            }
+        ];
+        const result = calculateTotal(listItems, 0, 0);
+        expect(result.subtotal).toBe(4);
+        expect(result.discount).toBe(0);
+        expect(result.tax).toBe(0);
+        expect(result.total).toBe(4);
+    });
 
-        it("excludes tax-exempt items for tax calculation", () =>{
+    it("calculates totals for multiple items", () => {
+        const listItems: CartItem[] = [
+            {
+                price: 4,
+                quantity: 1,
+            },
+            {
+                price: 3,
+                quantity: 1,
+            }
+        ];
+        const result = calculateTotal(listItems);
+        expect(result.subtotal).toBe(7);
+        expect(result.discount).toBe(0);
+        expect(result.tax).toBe(0);
+        expect(result.total).toBe(7);
 
-        });
+    });
 
-        // TODO: Add at leas 2 more test cases
+    it("applies discount before calculating tax", () => {
+        const listItems: CartItem[] = [
+            {
+                price: 4,
+                quantity: 1,
+            }
+        ];
+        const result = calculateTotal(listItems, 50, 50);
+        expect(result.subtotal).toBe(4);
+        expect(result.discount).toBe(2);
+        expect(result.tax).toBe(1);
+        expect(result.total).toBe(3);
+    });
+
+    it("excludes tax-exempt items for tax calculation", () => {
+        const listItems: CartItem[] = [
+            {
+                price: 4,
+                quantity: 1,
+                isTaxExempt: true,
+            },
+            {
+                price: 4,
+                quantity: 1,
+            }
+        ];
+        const result = calculateTotal(listItems, 0, 50);
+        expect(result.subtotal).toBe(8);
+        expect(result.discount).toBe(0);
+        expect(result.tax).toBe(2);
+        expect(result.total).toBe(10);
+    });
+
+    // TODO: Add at leas 2 more test cases
+    it("handles items with quantity greater then 1", () => {
+        const listItems: CartItem[] = [
+            {
+                price: 2,
+                quantity: 2,
+            }
+        ];
+        const result = calculateTotal(listItems);
+        expect(result.subtotal).toBe(4);
+        expect(result.discount).toBe(0);
+        expect(result.tax).toBe(0);
+        expect(result.total).toBe(4);
+    });
+
+    it("a ugly list of items with discount, tax and quantities", () => {
+        const listItems: CartItem[] = [
+            {
+                price: 2.99,
+                quantity: 1,
+            },
+            {
+                price: 3.89,
+                quantity: 3,
+            },
+            {
+                price: 1.50,
+                quantity: 3,
+                isTaxExempt: true
+            },
+            {
+                price: 5,
+                quantity: 1,
+            },
+            {
+                price: 3,
+                quantity: 2,
+            },
+            {
+                price: 1,
+                quantity: 4,
+                isTaxExempt: true
+            },
+        ];
+        const result = calculateTotal(listItems, 11, 8.5);
+        expect(result.subtotal).toBeCloseTo(34.16);
+        expect(result.discount).toBeCloseTo(3.76);
+        expect(result.tax).toBeCloseTo(1.94);
+        expect(result.total).toBeCloseTo(32.34);
+    });
+
+    it("handles my last walmart receipt", () => {
+        const listItems: CartItem[] = [
+            {
+                price: 5.86,
+                quantity: 1,
+            },
+            {
+                price: 9.51,
+                quantity: 1,
+                isTaxExempt: true
+            },
+            {
+                price: 1.98,
+                quantity: 4,
+                isTaxExempt: true
+            },
+            {
+                price: 1.32,
+                quantity: 1,
+            },
+            {
+                price: 4.97,
+                quantity: 1,
+            },
+            {
+                price: 8.12,
+                quantity: 1,
+            },
+            {
+                price: 8.27,
+                quantity: 2,
+            },
+        ];
+        const result = calculateTotal(listItems, 0, 8.8);
+        expect(result.subtotal).toBeCloseTo(54.24,1);
+        expect(result.discount).toBe(0);
+        expect(result.tax).toBeCloseTo(3.24,1);
+        expect(result.total).toBeCloseTo(57.48,1);
     });
 });
--- src/cartUtils.ts
+++ src/cartUtils.ts
@@ -22,8 +22,8 @@ export function applyDiscount(price: number, discountPercent: number):number {
         throw new Error("Discount cannot exceed 100%")
     }
 
-    const discountMultipiler = 1 - discountPercent / 100;
-    return price * discountMultipiler;
+    const discountMultiplier = 1 - discountPercent / 100;
+    return price * discountMultiplier;
 }
 
 export function calculateTax(
@@ -46,3 +46,26 @@ export function calculateTax(
     const tax = price * (taxRate / 100)
     return Math.round(tax * 100) / 100;
 }
+
+export function calculateTotal(
+    listItems: CartItem[],
+    discountPercent:number = 0,
+    taxRate:number = 0,
+): CartTotals {
+    const subtotal = listItems.reduce((sum, item) => sum + (item.price * item.quantity), 0);
+    // takes a sum of all items multiplied by their quantities
+    const discountPrices = listItems.map((item) => applyDiscount(item.price * item.quantity, discountPercent));
+    // creates and array of prices x quantities with the discount applied
+    const discount = subtotal - discountPrices.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    // finds the total discount by subtracting the discounted prices from the original subtotal
+    const tax = listItems.reduce((sum, item, currentIndex) => sum + calculateTax(discountPrices[currentIndex], taxRate, item.isTaxExempt), 0);
+    // finds the total tax by applying the taxRate to all discountedPrices
+    const total = subtotal - discount + tax;
+
+    return {
+        subtotal: Math.round(subtotal * 100)/100,
+        discount: Math.round(discount * 100)/100,
+        tax: Math.round(tax * 100)/100,
+        total: Math.round(total * 100)/100,
+    };
+};
